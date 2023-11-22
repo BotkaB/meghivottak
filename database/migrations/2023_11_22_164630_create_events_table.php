ff74914b0309182e779d2e4ff95879d341ccc636
BotkaB <botikabinaka@gmail.com>
2023-11-22 18:10:10 +0100

    adatbázis kialakítása

--- database/migrations/2023_11_22_164630_create_events_table.php
+++ database/migrations/2023_11_22_164630_create_events_table.php
@@ -11,13 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
+        Schema::create('events', function (Blueprint $table) {
+            $table->id('event_id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('sex');
-            $table->rememberToken();
+            $table->string('location');
+            $table->foreignId('org_id')->references('org_id')->on('organisators');
             $table->timestamps();
         });
     }
@@ -27,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('events');
     }
 };
--- database/migrations/2023_11_22_164701_create_invitations_table.php
+++ database/migrations/2023_11_22_164701_create_invitations_table.php
@@ -11,13 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('sex');
-            $table->rememberToken();
+        Schema::create('invitations', function (Blueprint $table) {
+            $table->id('invitation_id');
+            $table ->boolean('appeared')->default(0);
+            $table->foreignId('event_id')->references('event_id')->on('events');
+            $table->foreignId('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
@@ -27,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('invitations');
     }
 };
